64bb4d682a193d1fa5f7a7ef3c1fae2940e2f669
Kiru-axis <peter.kiru@student.moringaschool.com>
2021-06-19 19:40:30 +0300

    importing and initialising the needed modules

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,13 @@
 from flask import Flask
 from config import config_options
+from flask_bootstrap import Bootstrap
+from flask_sqlalchemy import SQLAlchemy
+from flask_mail import Mail
+from flask_login import LoginManager
+from flask_uploads import UploadSet, configure_uploads, IMAGES
+
+
+
 
 def create_app(config_name):
   app = Flask(__name__)
