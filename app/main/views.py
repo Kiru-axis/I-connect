970a3a662aa81ef0d05e8c927501089d1fc895fa
Kiru-axis <peter.kiru@student.moringaschool.com>
2021-06-19 21:15:47 +0300

    getting models classes to use in views

--- app/main/views.py
+++ app/main/views.py
@@ -1,7 +1,8 @@
 from flask import render_template,redirect,url_for,abort,request,flash
 from . import main
 from ..request import get_quotes
-
+from app.models import User,Blog,Comment,Subscriber
+from .. import db
 @main.route('/')
 def index():
   quotes = get_quotes()
