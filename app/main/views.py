828e4fe23f216a65a0ddef6e685eb46335d79150
Kiru-axis <peter.kiru@student.moringaschool.com>
2021-06-20 02:29:36 +0300

    setting and updating profile pictures

--- app/main/views.py
+++ app/main/views.py
@@ -15,4 +15,26 @@ def index():
     blogs = Blog.query.order_by(Blog.posted.desc()).paginate(page = page, per_page = 3)
     return render_template('index.html', quote = quotes,blogs=blogs)
 
+# Setting profile picture
+@main.route('/profile',methods = ['POST','GET'])
+def profile(uname):
+    user = User.query.filter_by(username = uname).first()
+
+    if user is None:
+        abort(404)
+
+    return render_template("profile/profile.html", user = user)
+
 # Updating profile
+@main.route('/user/<name>/updateprofile', methods = ['POST','GET'])
+@login_required
+def updateprofile(name):
+    form = UpdateProfile()
+    user = User.query.filter_by(username = name).first()
+    if user == None:
+        abort(404)
+    if form.validate_on_submit():
+        user.bio = form.bio.data
+        user.save()
+        return redirect(url_for('.profile',name = name))
+    return render_template('profile/updateprofile.html',form =form)
