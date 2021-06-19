4f3d1baa98f015d581c2b6f9797863e9b052fbb7
Kiru-axis <peter.kiru@student.moringaschool.com>
2021-06-19 20:12:35 +0300

    rendering quotes to markup

--- app/main/views.py
+++ app/main/views.py
@@ -1,7 +1,9 @@
 from flask import render_template,redirect,url_for,abort,request,flash
 from . import main
+from ..request import get_quotes
 
 @main.route('/')
 def index():
+  quotes = get_quotes()
   heading = 'Working.. Good to go'
-  return render_template('index.html', heading=heading)
+  return render_template('index.html', heading=heading,quote = quotes)
